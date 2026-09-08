feee48cbbc0896a40cb9f27c54b2565590e6abba
jaswanthkarjala10-tech <jaswanthkarjala10@gmail.com>
2026-09-08 09:59:38 +0530

    fix verify kyc button

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-import sqlite3
 from flask import Flask, render_template, request, redirect
+import sqlite3
+import os
 
 app = Flask(__name__)
+DB = 'kyc.db'
 
 def init_db():
-    conn = sqlite3.connect('kyc.db')
+    conn = sqlite3.connect(DB)
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS customers
-                 (id INTEGER PRIMARY KEY, name TEXT, country TEXT, risk TEXT, blocked INTEGER)''')
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  name TEXT, country TEXT, risk TEXT, blocked INTEGER)''')
     conn.commit()
     conn.close()
 
@@ -15,9 +18,9 @@ init_db()
 
 @app.route('/')
 def home():
-    conn = sqlite3.connect('kyc.db')
+    conn = sqlite3.connect(DB)
     c = conn.cursor()
-    c.execute("SELECT * FROM customers")
+    c.execute("SELECT * FROM customers ORDER BY id DESC")
     rows = c.fetchall()
     conn.close()
     return render_template('index.html', customers=rows)
@@ -26,14 +29,19 @@ def home():
 def add():
     name = request.form['name']
     country = request.form['country']
-    risk = 'HIGH' if country in ['Iran','North Korea','Syria'] else 'LOW'
+    # BNP Compliance Logic
+    high_risk = ['Iran','North Korea','Syria','Russia']
+    risk = 'HIGH' if country in high_risk else 'LOW'
     blocked = 1 if risk == 'HIGH' else 0
-    conn = sqlite3.connect('kyc.db')
+
+    conn = sqlite3.connect(DB)
     c = conn.cursor()
-    c.execute("INSERT INTO customers (name,country,risk,blocked) VALUES (?,?,?,?)", (name,country,risk,blocked))
+    c.execute("INSERT INTO customers (name,country,risk,blocked) VALUES (?,?,?,?)",
+              (name,country,risk,blocked))
     conn.commit()
     conn.close()
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
